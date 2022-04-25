6584703805e1ee31df0a6fa86327399835e32f25
Ortiz <jortiz@contractor.usgs.gov>
2022-04-25 18:57:39 -0500

    Created folder to store complete pipeline function. Created function that contains the 4 previously created functions and runs them to create one function that produces 4 outputs. Created script that runs this function.

---
+++ 4_pipeline/src/06_final_script.R
@@ -0,0 +1,5 @@
+# This script runs the pipeline created
+
+source('4_pipeline/src/05_data_to_4_visuals.R')
+project_output_dir = "D:/GitRepos_JO/ds-pipelines-targets-1/4_pipeline/out/"
+data_to_4_visuals(project_output_dir)
